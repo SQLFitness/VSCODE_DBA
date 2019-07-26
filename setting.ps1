9842fa2faa8fe9d033aadf99728832426ea9b809
Steven Peterson <speterso@1800contacts.com>
2019-07-26 16:26:16 -0600

    first commit

---
+++ setting.ps1
@@ -0,0 +1,7 @@
+##############################################################################################
+## change the size of the fonts in VS Code editor and terminal
+##############################################################################################
+#texteditor.font.size
+#terminal.integrated.shell.windows #changes
+
+#font.size
