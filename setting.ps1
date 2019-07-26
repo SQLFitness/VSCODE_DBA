f1aa9fe614d283f08ce7e4e3db0cf8dd0968e135
Steven Peterson <speterso@1800contacts.com>
2019-07-26 17:11:16 -0600

    rming crap

--- setting.ps1
+++ setting.ps1
@@ -5,3 +5,8 @@
 #terminal.integrated.shell.windows #changes
 
 #font.size
+
+Install-Module dbatools -MaximumVersion 0.9.752 
+Install-Module dbatools -MaximumVersion 0.9.752 -scope CurrentUser
+
+get-module
