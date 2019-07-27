dd6fed2e41276d4e4988a6123b809fde090c8616
Steven Peterson <speterso@1800contacts.com>
2019-07-27 13:39:13 -0600

    database docker

---
+++ docker/docker_2019.ps1
@@ -0,0 +1,20 @@
+docker pull mcr.microsoft.com/mssql/server:2019-CTP2.5-ubuntu
+
+docker run `
+--name DEVSQL19 `
+-p 1433:1433 `
+-e "ACCEPT_EULA=Y" `
+-e "SA_PASSWORD=!QAZ2wsx" `
+-v C:\temp\Docker\SQL:/sqldev `
+-d mcr.microsoft.com/mssql/server:2019-CTP2.5-ubuntu
+
+# cleanup 
+# docker ps -aq
+# docker rm d9f
+# docker rm $(docker ps -aq) #removes containers not images
+
+docker ps -a #ps stands for Process Start and -a will you show you all processes regardless of status
+
+docker start DEVSQL19
+
+docker stop DEVSQL19
