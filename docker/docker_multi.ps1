ba4f0d4b1ccd4be270753dcfacbe1a32c6406e7e
Steven Peterson <speterso@1800contacts.com>
2019-07-30 09:39:48 -0600

    ASd

--- docker/docker_multi.ps1
+++ docker/docker_multi.ps1
@@ -21,16 +21,24 @@ $sqlservers
 ## make the continers
 ##############################################################################################
 
+docker rmi mcr.microsoft.com/mssql/server:2019-CTP2.5-ubuntu
+docker pull mcr.microsoft.com/mssql/server:2019-CTP3.2-ubuntu
+
 foreach($sqlserver in $sqlservers) 
 {
     $port = $sqlserver -replace "\D", ""
 
-    docker run --name $sqlserver -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=!QAZ2wsx" -p $port`:1433 -v C:\temp\Docker\SQL:/sql -d mcr.microsoft.com/mssql/server:2019-CTP2.5-ubuntu
+    "$port = " + $port
+
+    docker run --name $sqlserver -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=!QAZ2wsx" -p $port`:1433 -d mcr.microsoft.com/mssql/server:2019-CTP3.2-ubuntu
+    #-v C:\temp\Docker\SQL:/sql 
     docker start $sqlserver
 }
 
-# docker run --name SQL1401 -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=!QAZ2wsx" -p 1401:1433 -v C:\temp\Docker\SQL1401:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2017-latest
+# docker run --name SQL1401 -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=!QAZ2wsx" -p 1433:1433 -v C:\temp\Docker\SQL:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2019-CTP3.2-ubuntu
 # docker run --name SQL1402 -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=!QAZ2wsx" -p 1402:1433 -v C:\temp\Docker\SQL1402:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2017-latest
+
+docker start SQL1401
 
 # docker images
 # docker container ls -a
@@ -47,6 +55,7 @@ docker start $(docker ps -aq -f status=exited)
 # docker rm $(docker ps -aq)
 
 docker ps -a  
+docker port  SQL1401
 
 # docker start SQL1402
 # docker start infallible_buck
@@ -56,3 +65,8 @@ docker ps -a
 
 # docker rm 3c0
 # docker rm 6d7
+
+sqlcmd -S 0.0.0.0,1401 -U SA -P !QAZ2wsx
+docker-machine ls
+
+docker logs e202645a0fba
