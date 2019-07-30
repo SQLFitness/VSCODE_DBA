ada707eb834eb8d4df0558e6c1f0e10f5d44db2b
Steve Peterson <drblind@gmail.com>
2019-07-30 15:09:20 -0600

    sanity check

---
+++ docker/gettingstarted.ps1
@@ -0,0 +1,24 @@
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=<YourStrong!Passw0rd>" `
+   -p 1433:1433 --name sql1 `
+   -d mcr.microsoft.com/mssql/server:2017-latest
+
+docker ps -a
+
+docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd `
+   -S localhost -U SA -P "!QAZ2wsx" `
+   -Q "ALTER LOGIN SA WITH PASSWORD='1QAZ2wsx'"
+
+
+##############################################################################################
+## bash
+##############################################################################################
+docker exec -it sql1 "bash"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "1QAZ2wsx"
+
+##############################################################################################
+## powershell
+##############################################################################################
+sqlcmd -S localhost -U SA -P "1QAZ2wsx"
+
+docker stop sql1
+docker rm sql1
