3dadf61ab27821be786c72c4fd0e07850ca502aa
Steven Peterson <speterso@1800contacts.com>
2019-08-02 22:35:52 -0600

    first edit

--- Database/02.TurnUpTheHeat.ps1
+++ Database/02.TurnUpTheHeat.ps1
@@ -1,7 +1,8 @@
 $sqlcred = Get-Credential
 $sqlinst = "localhost,1401"
 $dbname = "VSDBA"
-$csvfile = ".\files\SampleCSVFile_5300kb.csv"
+#$csvfile = ".\files\SampleCSVFile_5300kb.csv"
+$csvfile = ".\files\SampleCSVFile_556kb.csv"
 
 Import-DbaCsv -SqlInstance $sqlinst -Database $dbname -SqlCredential $sqlcred -Csv $csvfile -Table SampleCSV -AutoCreateTable 
 
